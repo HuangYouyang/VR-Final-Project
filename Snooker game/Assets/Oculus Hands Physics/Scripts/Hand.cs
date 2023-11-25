04fb97c329fec9e51001d6ba45f9bcdbc1ec52ea
HuangYouyang <u1419187@umail.utah.edu>
2023-11-24 21:24:20 -0700

    physics hands

--- Snooker game/Assets/Oculus Hands Physics/Scripts/Hand.cs
+++ Snooker game/Assets/Oculus Hands Physics/Scripts/Hand.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
-    public float speed;
+    public float animationSpeed;
     Animator animator;
     private float gripTarget;
     private float triggerTarget;
@@ -14,16 +14,46 @@ public class Hand : MonoBehaviour
     private string animatorGripParam = "Grip";
     private string animatorTriggerParam = "Trigger";
 
+    // Physics Movement
+    [SerializeField] private GameObject followObject;
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float rotateSpeed = 100f;
+    private Transform followTarget;
+    private Rigidbody body;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Animation
         animator = GetComponent<Animator>();
+
+        // Physics Movement
+        followTarget = followObject.transform;
+        body = GetComponent<Rigidbody>();
+        body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        body.interpolation = RigidbodyInterpolation.Interpolate;
+        body.mass = 20f;
+
+        // Teleport hands
+        body.position = followTarget.position;
+        body.rotation = followTarget.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        AnimateHand();
+        AnimateHand(); // animation
+
+        PhysicsMove();
+    }
+
+    private void PhysicsMove()
+    { 
+        // Position
+        var distance = Vector3.Distance(followTarget.position, transform.position);
+        body.velocity = (followTarget.position - transform.position).normalized * (followSpeed * distance);
+
+        // Rotation
     }
 
     internal void SetGrip(float v)
@@ -41,12 +71,12 @@ public class Hand : MonoBehaviour
     {
         if(gripCurrent!=gripTarget)
         {
-            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * animationSpeed);
             animator.SetFloat(animatorGripParam, gripCurrent);
         }
         if(triggerCurrent!=triggerTarget)
         {
-            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * animationSpeed);
             animator.SetFloat(animatorTriggerParam, triggerCurrent);
         }
     }
