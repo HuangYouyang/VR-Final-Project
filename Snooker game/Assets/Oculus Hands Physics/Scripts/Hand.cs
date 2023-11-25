382114dfbf49f15c8001f8bb47e59e36bdf682ef
HuangYouyang <u1419187@umail.utah.edu>
2023-11-25 09:23:35 -0700

    physics hands finished

--- Snooker game/Assets/Oculus Hands Physics/Scripts/Hand.cs
+++ Snooker game/Assets/Oculus Hands Physics/Scripts/Hand.cs
@@ -18,6 +18,8 @@ public class Hand : MonoBehaviour
     [SerializeField] private GameObject followObject;
     [SerializeField] private float followSpeed = 30f;
     [SerializeField] private float rotateSpeed = 100f;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Vector3 rotationOffset;
     private Transform followTarget;
     private Rigidbody body;
 
@@ -40,7 +42,7 @@ public class Hand : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         AnimateHand(); // animation
 
@@ -48,18 +50,27 @@ public class Hand : MonoBehaviour
     }
 
     private void PhysicsMove()
-    { 
+    {
         // Position
-        var distance = Vector3.Distance(followTarget.position, transform.position);
+        var positionWithOffset = followTarget.position + positionOffset;
+        var distance = Vector3.Distance(positionWithOffset, transform.position);
         body.velocity = (followTarget.position - transform.position).normalized * (followSpeed * distance);
 
         // Rotation
-    }
+        var rotationWithOffset = followTarget.rotation * Quaternion.Euler(rotationOffset);
+        var q = rotationWithOffset * Quaternion.Inverse(body.rotation);
+        q.ToAngleAxis(out float angle, out Vector3 axis);
+        body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
+        if (Mathf.Abs(axis.magnitude) != Mathf.Infinity)
+        {
+            if (angle > 180.0f) { angle -= 360.0f; }
+            body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
+        }
+    }   
 
     internal void SetGrip(float v)
     {
         gripTarget = v;
-        Debug.Log(gripTarget);
     }
 
     internal void SetTrigger(float v)
